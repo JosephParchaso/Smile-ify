724f5a3650691003354e20820f6c8d8f30742be5
18100807 <josephparchaso@gmail.com>
2025-06-15 02:54:14 +0800

    toggleNavbar

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,6 @@ $basePath = '/Smile-ify';
 
     <link rel="icon" href="<?= $basePath ?>/images/logo/logo_white.png" type="image/x-icon">
     <link rel="stylesheet" href="<?= $basePath ?>/css/style.css?v=<?= time(); ?>">
-    <script src="<?= $basePath ?>/js/navbarToggle.js?v=<?= time(); ?>"></script>
+    <script src="<?= $basePath ?>/js/toggleNavbar.js?v=<?= time(); ?>"></script>
 </head>
     
