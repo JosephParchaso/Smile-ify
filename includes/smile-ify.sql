fec2a556c87be1ccb56870006d6e89a7325c3f08
18100807 <josephparchaso@gmail.com>
2025-07-12 08:07:57 +0800

    sql

---
+++ includes/smile-ify.sql
@@ -0,0 +1,418 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2025 at 02:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smile-ify`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment_transaction`
+--
+
+CREATE TABLE `appointment_transaction` (
+  `appointment_transaction_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `dentist_id` int(11) DEFAULT NULL,
+  `appointment_date` date NOT NULL,
+  `appointment_time` time NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointment_transaction`
+--
+
+INSERT INTO `appointment_transaction` (`appointment_transaction_id`, `user_id`, `branch_id`, `service_id`, `dentist_id`, `appointment_date`, `appointment_time`, `date_created`) VALUES
+(1, 10, 3, 5, 4, '2025-07-18', '15:00:00', '2025-07-11 22:31:48'),
+(2, 11, 3, 8, NULL, '2025-07-18', '12:00:00', '2025-07-11 22:36:13'),
+(3, 12, 1, 1, 3, '2025-07-18', '09:00:00', '2025-07-11 23:10:37'),
+(4, 13, 1, 1, 3, '2025-08-07', '13:30:00', '2025-07-11 23:49:00'),
+(5, 14, 1, 1, 3, '2025-07-18', '09:00:00', '2025-07-11 23:53:50'),
+(6, 15, 1, 5, 3, '2025-07-19', '12:45:00', '2025-07-11 23:57:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `branch_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`branch_id`, `name`) VALUES
+(1, 'Babag'),
+(2, 'Pusok'),
+(3, 'Mandaue');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch_service`
+--
+
+CREATE TABLE `branch_service` (
+  `branch_services_id` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branch_service`
+--
+
+INSERT INTO `branch_service` (`branch_services_id`, `branch_id`, `service_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
+(5, 1, 5),
+(6, 2, 6),
+(7, 2, 7),
+(8, 2, 8),
+(9, 2, 9),
+(10, 2, 10),
+(11, 3, 1),
+(12, 3, 2),
+(13, 3, 3),
+(14, 3, 4),
+(15, 3, 5),
+(16, 3, 6),
+(17, 3, 7),
+(18, 3, 8),
+(19, 3, 9),
+(20, 3, 10),
+(21, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dentist`
+--
+
+CREATE TABLE `dentist` (
+  `dentist_id` int(11) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `gender` enum('male','female') NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact_number` varchar(10) NOT NULL,
+  `license_number` varchar(50) NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dentist`
+--
+
+INSERT INTO `dentist` (`dentist_id`, `last_name`, `middle_name`, `first_name`, `gender`, `email`, `contact_number`, `license_number`, `status`, `date_created`) VALUES
+(1, 'Ulanday', '', 'Precious', 'female', 'precious@gmail.com', '0912345678', 'LIC-1001', 'active', '2025-07-11 19:40:26'),
+(2, 'Chan', 'Marie', 'Kyla', 'female', 'kyla.marie@gmail.com', '0912345679', 'LIC-1002', 'active', '2025-07-11 19:40:26'),
+(3, 'Summers', '', 'Daze', 'female', 'daze@gmail.com', '0912345680', 'LIC-1003', 'active', '2025-07-11 19:40:26'),
+(4, 'San Jose', '', 'Solene', 'female', 'solene@gmail.com', '0912345681', 'LIC-1004', 'active', '2025-07-11 19:40:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dentist_branch`
+--
+
+CREATE TABLE `dentist_branch` (
+  `dentist_branch_id` int(11) NOT NULL,
+  `dentist_id` int(11) DEFAULT NULL,
+  `branch_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dentist_branch`
+--
+
+INSERT INTO `dentist_branch` (`dentist_branch_id`, `dentist_id`, `branch_id`) VALUES
+(1, 1, 1),
+(2, 1, 3),
+(3, 2, 2),
+(4, 3, 1),
+(5, 3, 2),
+(6, 4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dentist_service`
+--
+
+CREATE TABLE `dentist_service` (
+  `dentist_services_id` int(11) NOT NULL,
+  `dentist_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dentist_service`
+--
+
+INSERT INTO `dentist_service` (`dentist_services_id`, `dentist_id`, `service_id`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 2, 3),
+(4, 2, 6),
+(5, 3, 1),
+(6, 3, 2),
+(7, 3, 3),
+(8, 3, 4),
+(9, 3, 5),
+(10, 3, 6),
+(11, 3, 7),
+(12, 3, 8),
+(13, 3, 9),
+(14, 3, 10),
+(15, 4, 5),
+(16, 4, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `service_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `status` enum('active','inactive') NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`service_id`, `name`, `status`, `date_created`) VALUES
+(1, 'Consultation', 'active', '2025-07-11 19:08:44'),
+(2, 'Tooth Extraction', 'active', '2025-07-11 19:08:44'),
+(3, 'Dental Filling', 'active', '2025-07-11 19:08:44'),
+(4, 'Root Canal Treatment', 'active', '2025-07-11 19:08:44'),
+(5, 'Dental Crown Placement', 'active', '2025-07-11 19:08:44'),
+(6, 'Orthodontic Braces', 'active', '2025-07-11 19:08:44'),
+(7, 'Teeth Whitening', 'active', '2025-07-11 19:08:44'),
+(8, 'Complete Denture', 'active', '2025-07-11 19:08:44'),
+(9, 'Partial Denture', 'active', '2025-07-11 19:08:44'),
+(10, 'Dental Implant', 'active', '2025-07-11 19:08:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `gender` enum('male','female') NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact_number` varchar(10) NOT NULL,
+  `role` enum('owner','admin','patient') NOT NULL,
+  `status` enum('active','inactive') NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `last_name`, `middle_name`, `first_name`, `gender`, `email`, `contact_number`, `role`, `status`, `date_created`) VALUES
+(1, 'owner01', '$2a$12$tpxGgFFwdB5hG7rOus1ZuuQzJy8xwA4kvaUTZdvMqCTB9dK/v0yFS', 'Owner', 'Dummy', 'Sample', 'male', '', '9998887777', 'owner', 'active', '2025-06-14 10:39:32'),
+(2, 'admin01', '$2a$12$pKUNknha3XVdUTPXL84XUe8YxjAnrLmF7ZeB8KUspXb.ouNVrFGt6', 'Admin', 'Dummy', 'Sample', 'female', '', '9223334444', 'admin', 'active', '2025-06-14 10:39:32'),
+(3, 'patient01', '$2a$12$RJZbVUZ3JDsUjDB5eFTyiuACuCvxFJyrQI4cE9u8fQ2ChJf/.Srdq', 'Patient', 'Dummy', 'Sample', 'male', '', '9123456789', 'patient', 'active', '2025-06-14 10:39:32'),
+(8, 'ParchasoJ', '$2y$10$9fqT9Gco1CtAKVKu6bSDgeh9SFZbg/bb8GJR5OJrrVIJycPZgqxWC', 'Parchaso', 'Espana', 'Jhon', 'male', '9055626239', 'josephparc', 'owner', 'active', '2025-07-11 22:27:56'),
+(9, 'ParchasoJJ', '$2y$10$W.BXzGapcq9J/oxpk1RHYeaYMxBCz0fSTp8YiRh41YSUebQ12Hjhy', 'Parchaso', 'Joseph', 'Jhon', 'male', '9055626239', 'josephparc', 'owner', 'active', '2025-07-11 22:30:07'),
+(10, 'ParchasoJo', '$2y$10$7TtiXyp5duA0k9e0t7nmX.BfdQgpQkYwzUVT8tiCFDY166aCIZFoG', 'Parchaso', '', 'Joseph', 'male', '9055626239', 'josephparc', 'patient', 'active', '2025-07-11 22:31:48'),
+(11, 'PototR', '$2y$10$2LodhIAZ8iTEc7e5U8ciM.ERBdKf5t2XTv1enJGrQyxJC501X7DG.', 'Potot', 'Travero', 'Rix', 'female', '9950217941', 'josephparc', 'patient', 'active', '2025-07-11 22:36:13'),
+(12, 'ParchasoJh', '$2y$10$V9IluHOclcF/fELFexGVPOwsPpXLMCpVz6X5tA53PNxqjGUN39XHq', 'Parchaso', 'Espana', 'Jhon', 'male', 'josephparchaso@gmail.com', '9055626239', 'patient', 'active', '2025-07-11 23:10:37'),
+(13, 'asdasdaa', '$2y$10$xx/z0aCzxsgWfhbTyNeT2ewgqLJDF0eA4MIG2KJAh48nLldnoT7mu', 'asdasda', 'dasdads', 'asdasdas', 'male', 'josephparchaso@gmail.com', '3543535353', 'patient', 'active', '2025-07-11 23:49:00'),
+(14, 'ParchasoJn', '$2y$10$n0Wn8ENlBKZSmFAc8XIo6.8MBB9.vBHC9jj20HU4E4aBg3QzLnJGa', 'Parchaso', 'espana', 'Jhon', 'male', 'josephparchaso@gmail.com', '9055626239', 'patient', 'active', '2025-07-11 23:53:50'),
+(15, 'pototrr', '$2y$10$81epq2ks0nZntMd5BJtQZeTQgwcM1olJw54tWvY3BCS2.WMKgVy6q', 'potot', 'travs', 'rix', 'female', 'josephparchaso@gmail.com', '3242424243', 'patient', 'active', '2025-07-11 23:57:34');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment_transaction`
+--
+ALTER TABLE `appointment_transaction`
+  ADD PRIMARY KEY (`appointment_transaction_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `branch_id` (`branch_id`),
+  ADD KEY `service_id` (`service_id`),
+  ADD KEY `dentist_id` (`dentist_id`);
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`branch_id`);
+
+--
+-- Indexes for table `branch_service`
+--
+ALTER TABLE `branch_service`
+  ADD PRIMARY KEY (`branch_services_id`),
+  ADD KEY `fk_branch` (`branch_id`),
+  ADD KEY `fk_service` (`service_id`);
+
+--
+-- Indexes for table `dentist`
+--
+ALTER TABLE `dentist`
+  ADD PRIMARY KEY (`dentist_id`);
+
+--
+-- Indexes for table `dentist_branch`
+--
+ALTER TABLE `dentist_branch`
+  ADD PRIMARY KEY (`dentist_branch_id`),
+  ADD KEY `dentist_id` (`dentist_id`),
+  ADD KEY `branch_id` (`branch_id`);
+
+--
+-- Indexes for table `dentist_service`
+--
+ALTER TABLE `dentist_service`
+  ADD PRIMARY KEY (`dentist_services_id`),
+  ADD KEY `dentist_id` (`dentist_id`),
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`service_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `index_username_unique` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment_transaction`
+--
+ALTER TABLE `appointment_transaction`
+  MODIFY `appointment_transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `branch`
+--
+ALTER TABLE `branch`
+  MODIFY `branch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `branch_service`
+--
+ALTER TABLE `branch_service`
+  MODIFY `branch_services_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `dentist`
+--
+ALTER TABLE `dentist`
+  MODIFY `dentist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `dentist_branch`
+--
+ALTER TABLE `dentist_branch`
+  MODIFY `dentist_branch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `dentist_service`
+--
+ALTER TABLE `dentist_service`
+  MODIFY `dentist_services_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment_transaction`
+--
+ALTER TABLE `appointment_transaction`
+  ADD CONSTRAINT `appointment_transaction_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `appointment_transaction_ibfk_2` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`),
+  ADD CONSTRAINT `appointment_transaction_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`),
+  ADD CONSTRAINT `appointment_transaction_ibfk_4` FOREIGN KEY (`dentist_id`) REFERENCES `dentist` (`dentist_id`);
+
+--
+-- Constraints for table `branch_service`
+--
+ALTER TABLE `branch_service`
+  ADD CONSTRAINT `fk_branch` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_service` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `dentist_branch`
+--
+ALTER TABLE `dentist_branch`
+  ADD CONSTRAINT `dentist_branch_ibfk_1` FOREIGN KEY (`dentist_id`) REFERENCES `dentist` (`dentist_id`),
+  ADD CONSTRAINT `dentist_branch_ibfk_2` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`);
+
+--
+-- Constraints for table `dentist_service`
+--
+ALTER TABLE `dentist_service`
+  ADD CONSTRAINT `dentist_service_ibfk_1` FOREIGN KEY (`dentist_id`) REFERENCES `dentist` (`dentist_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `dentist_service_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
